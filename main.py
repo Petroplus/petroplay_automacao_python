da2d7ac1f607bafd094bebb8e7bd0130a9032bfd
Hallan Abreu <jotahagar@gmail.com>
2025-10-08 12:02:04 -0300

    update

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import uvicorn
-import os
 from fastapi import FastAPI
-
 
 from routes_socket import RoutesSocket
 
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000)) 
     app = FastAPI()
     routesSocket = RoutesSocket(app)
     routesSocket.register_routes()
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
