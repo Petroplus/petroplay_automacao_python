f37a94b37707824ae2ac0e0aa99bf40fa950e911
Hallan Abreu <jotahagar@gmail.com>
2025-10-21 17:28:32 -0300

    initialize llm

--- app/feature/get_plate_socket/find_plate_in_image_bgr.py
+++ app/feature/get_plate_socket/find_plate_in_image_bgr.py
@@ -1,22 +1,22 @@
-import easyocr
-import re
-import cv2
+# import easyocr
+# import re
+# import cv2
 
-class Find_plate_in_image_bgr:
-    def __init__(self):
-        self.ocr_reader = easyocr.Reader(['pt', 'en'], gpu=False) 
-        self.PLATE_REGEX = re.compile(r'\b([A-Z]{3}\d{4}|[A-Z]{3}\d[A-Z]\d{2})\b')
+# class Find_plate_in_image_bgr:
+#     def __init__(self):
+#         self.ocr_reader = easyocr.Reader(['pt', 'en'], gpu=False) 
+#         self.PLATE_REGEX = re.compile(r'\b([A-Z]{3}\d{4}|[A-Z]{3}\d[A-Z]\d{2})\b')
 
-    def initialize(self, img_bgr):
-        img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-        results = self.ocr_reader.readtext(img_rgb)
-        candidates = []
-        for bbox, text, prob in results:
-            t = re.sub(r'[^A-Z0-9]', '', text.upper())
-            m = self.PLATE_REGEX.search(t)
-            if m:
-                candidates.append((m.group(1), float(prob)))
-        if not candidates:
-            return None
-        candidates.sort(key=lambda x: x[1], reverse=True)
-        return candidates[0] 
+#     def initialize(self, img_bgr):
+#         img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+#         results = self.ocr_reader.readtext(img_rgb)
+#         candidates = []
+#         for bbox, text, prob in results:
+#             t = re.sub(r'[^A-Z0-9]', '', text.upper())
+#             m = self.PLATE_REGEX.search(t)
+#             if m:
+#                 candidates.append((m.group(1), float(prob)))
+#         if not candidates:
+#             return None
+#         candidates.sort(key=lambda x: x[1], reverse=True)
+#         return candidates[0] 
--- app/feature/get_plate_socket/get_plate_socket.py
+++ app/feature/get_plate_socket/get_plate_socket.py
@@ -1,19 +1,17 @@
 import base64
 from fastapi import WebSocket
-import numpy as np
-import cv2
+# import numpy as np
 import json
-from app.feature.get_plate_socket.connection_state import ConnectionState
+import asyncio
 from app.feature.get_plate_socket.datasource.validate_token import ValidateToken
-from app.feature.get_plate_socket.find_plate_in_image_bgr import Find_plate_in_image_bgr
+from app.feature.get_plate_socket.llm import GetPlateLLm
 
 
 class Websocket_plate_detector:
     async def init_socket(self, ws: WebSocket):
         await ws.accept()
         print("✅ Cliente conectado")
-        find_plate_in_image_bgr = Find_plate_in_image_bgr()
-        state = ConnectionState(consensus_frames=4)
+        cont = 0
         try:
             data_text = await ws.receive_text()
 
@@ -21,28 +19,33 @@ class Websocket_plate_detector:
                 data = json.loads(data_text)
             except json.JSONDecodeError:
                 print("❌ Erro: JSON inválido na primeira mensagem")
+                await ws.send_text(json.dumps({"error": "JSON inválido na primeira mensagem"}))
                 await ws.close(code=4000)
                 return
             
             token = data.get("token")
             if not token:
                 print("❌ Token ausente — conexão encerrada")
+                await ws.send_text(json.dumps({"token": False}))
+                await ws.send_text(json.dumps({"error": "Sem permissão = 401"}))
                 await ws.close(code=4001)
                 return
             else:
                 validateToken = ValidateToken()
                 response = validateToken.token(token)
+                await asyncio.sleep(1)
                 if response != 200:
                     print("❌ Token ausente — conexão encerrada")
+                    await ws.send_text(json.dumps({"error": "Erro ao validar o token"}))
                     await ws.close(code=4001)
                     return
 
             print(f"🔐 Token recebido: {token}")
-            await ws.send_text('true')
+            await ws.send_text(json.dumps({"token": True}))
 
             while True:
+                await asyncio.sleep(1)
                 data = await ws.receive_text()
-
                 print("📩 Frame recebido")
                 payload = json.loads(data)
                 b64 = payload.get("frame")
@@ -50,26 +53,35 @@ class Websocket_plate_detector:
                     print("⚠️ Nenhum frame encontrado no payload")
                     await ws.send_text(json.dumps({"error":"no_frame"}))
                     continue
+
                 header_removed = b64.split(",")[-1]
                 img_bytes = base64.b64decode(header_removed)
-                arr = np.frombuffer(img_bytes, np.uint8)
-                img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
-                if img is None:
-                    print("⚠️ Imagem inválida")
-                    await ws.send_text(json.dumps({"error":"invalid_image"}))
-                    continue
-                found = find_plate_in_image_bgr.initialize(img)
-                plate = found[0] if found else None
-                print(f"🔍 Detecção OCR: {plate}")
 
-                state.push(plate)
-                confirmed = state.consensus_plate()
-                if confirmed:
-                    print(f"✅ Placa confirmada: {confirmed}")
-                    await ws.send_text(json.dumps({"plate": confirmed}))
-                    state.last_detections.clear()
+                returnPlate = await GetPlateLLm.detect_plate_from_image(img_bytes)
+
+                # returnPlate = {'plate': None}
+
+                if returnPlate['plate'] is not None:
+                    await ws.send_text(json.dumps({"plate": returnPlate['plate']}))
+                    print(f"Placa detectada: {returnPlate['plate']}, encerrando loop")
+                    break
                 else:
-                    await ws.send_text(json.dumps({"detected": plate}))
+                    await ws.send_text(json.dumps({"noDetected": 'none'}))
+
+
+
+
+                await asyncio.sleep(1)
+
+                cont = cont + 1
+
+                if cont > 3:
+                # if cont > 30:
+                    await ws.send_text(json.dumps({"error": "Quantidade de tentativas excedida"}))
+                    await ws.close()
+                    break
+                    
         except Exception as e:
             print(f"❌ Erro WebSocket: {e}")
+            await ws.send_text(json.dumps({"error": "Erro desconhecido"}))
             await ws.close()
